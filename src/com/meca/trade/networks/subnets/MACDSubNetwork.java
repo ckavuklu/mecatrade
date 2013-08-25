60f0ee52303b5362b8484ced16482a3e95d7b3ec
ckavuklu <celal.kavuklu@yandex.com>
2013-08-25 18:05:07 +0300

    In progress Subnet works

--- src/com/meca/trade/networks/subnets/MACDSubNetwork.java
+++ src/com/meca/trade/networks/subnets/MACDSubNetwork.java
@@ -1,15 +1,32 @@
 package com.meca.trade.networks.subnets;
 
+import com.jpmorrsn.fbp.engine.InPort;
+import com.jpmorrsn.fbp.engine.InPorts;
+import com.jpmorrsn.fbp.engine.OutPort;
 import com.jpmorrsn.fbp.engine.SubNet;
+import com.meca.trade.to.TradeData;
 
+@OutPorts({
+	@OutPort(value = "PRICETYPE", description = "type", type = String.class),
+	@OutPort(value = "TRADEDATA", description = "trade data", type = TradeData.class) })
+@InPort("IN")
 public class MACDSubNetwork extends SubNet {
 
 	@Override
 	  protected void define() {
 	    
 		
-
+		
+		 component("SUBIN", com.jpmorrsn.fbp.engine.SubInComponent.class); 
+		 component("SUBOUT", com.jpmorrsn.fbp.engine.SubOutComponent.class); // do.
+		    
+		 
+	    initialize("IN", component("SUBIN"), port("IN",0));
 	    
+	    initialize("OUT", component("SUBOUT"), port("OUT",0));
+		    
+		    
+		
 	    // Indicator Components
 	    component("_ExponentialMovingAverage", com.meca.trade.components.ExponentialMovingAverage.class);
 	    
