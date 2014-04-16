49c1c48daa38cbd9c39f3d7e2eceba735586c689
madacal <madacal@gmail.com>
2014-04-16 22:20:48 +0300

    ParabolicSAR will be integrated into system later on.

---
+++ output/graphtemplate/Main.java
@@ -0,0 +1,115 @@
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		
+		ParabolicSAR sar = new ParabolicSAR(0.02, 0.20, 1);
+		
+		sar.execute(46.59d, 45.90d);
+		sar.execute(46.55d, 45.38d);
+		sar.execute(46.30d, 45.25d);
+		sar.execute(45.43d, 43.99d);
+		sar.execute(44.55d, 44.07d);
+		sar.execute(44.84d, 44.00d);
+		sar.execute(44.80d, 43.96d);
+		sar.execute(44.38d, 43.27d);
+		sar.execute(43.97d, 42.58d);
+		sar.execute(43.23d, 42.83d);
+		sar.execute(43.73d, 42.98d);
+		sar.execute(43.92d, 43.37d);
+		sar.execute(43.61d, 42.57d);
+		sar.execute(42.97d, 42.07d);
+		sar.execute(43.13d, 42.59d);
+		sar.execute(43.46d, 42.71d);
+		sar.execute(43.26d, 42.70d);
+		sar.execute(43.74d, 42.71d);
+		sar.execute(43.83d, 43.11d);
+		sar.execute(44.30d, 43.80d);
+		sar.execute(44.52d, 44.21d);
+		sar.execute(44.88d, 44.40d);
+		sar.execute(45.00d, 44.57d);
+		sar.execute(44.98d, 44.51d);
+		sar.execute(44.69d, 43.90d);
+		sar.execute(44.74d, 44.27d);
+		sar.execute(44.65d, 43.78d);
+		sar.execute(44.81d, 44.35d);
+		sar.execute(44.81d, 44.35d);
+		sar.execute(45.44d,	44.90d);
+		sar.execute(45.80d, 45.40d);
+		sar.execute(45.77d,	45.38d);
+		sar.execute(45.76d, 45.35d);
+		sar.execute(46.49d,	45.94d);
+		sar.execute(46.59d,	46.39d);
+		sar.execute(47.00d,	46.35d);
+		sar.execute(47.23d,	46.75d);
+		sar.execute(47.30d,	46.93d);
+		sar.execute(47.48d,	47.05d);
+		sar.execute(47.33d,	46.86d);
+		sar.execute(47.56d,	47.18d);
+		sar.execute(47.85d,	47.48d);
+		sar.execute(47.83d,	47.55d);
+		sar.execute(47.95d,	47.32d);
+		sar.execute(48.11d,	47.25d);
+		sar.execute(48.30d,	47.77d);
+		sar.execute(48.17d,	47.91d);
+		sar.execute(48.60d,	47.90d);
+		sar.execute(48.33d,	47.74d);
+		
+		
+		/*
+		
+		StochasticOscillator sO = new StochasticOscillator(14, 0, 3);
+		sO.execute(127.01d, 125.36d, 125.36d);
+		sO.execute(127.62d, 126.16d, 126.16d);
+		sO.execute(126.59d, 124.93d, 126.16d);
+		sO.execute(127.35d, 126.09d, 126.16d);
+		sO.execute(128.17d, 126.82d, 126.16d);
+		sO.execute(128.43d, 126.48d, 126.16d);
+		sO.execute(127.37d, 126.03d, 126.16d);
+		sO.execute(126.42d, 124.83d, 126.16d);
+		sO.execute(126.90d, 126.39d, 126.16d);
+		sO.execute(126.85d, 125.72d, 126.16d);
+		sO.execute(125.65d, 124.56d, 126.16d);
+		
+		sO.execute(125.72d, 124.57d, 126.16d);
+		sO.execute(127.16d, 125.07d, 126.16d);
+		sO.execute(127.72d, 126.86d, 127.29d);
+		sO.execute(127.69d, 126.63d, 127.18d);
+		sO.execute(128.22d, 126.80d, 128.01d);
+		sO.execute(128.27d, 126.71d, 127.11d);
+		sO.execute(128.09d, 126.80d, 127.73d);
+		
+		*/
+		/*RelativeStrengthIndex rsi = new RelativeStrengthIndex(14);
+		
+		
+		rsi.execute(44.34d);
+		rsi.execute(44.09d);
+		rsi.execute(44.15d);
+		rsi.execute(43.61d);
+		rsi.execute(44.33d);
+		rsi.execute(44.83d);
+		rsi.execute(45.10d);
+		rsi.execute(45.42d);
+		rsi.execute(45.84d);
+		rsi.execute(46.08d);
+		rsi.execute(45.89d);
+		rsi.execute(46.03d);
+		rsi.execute(45.61d);
+		rsi.execute(46.28d);
+		rsi.execute(46.28d);
+		rsi.execute(46.0d);
+		rsi.execute(46.03d);
+		rsi.execute(46.41d);
+		rsi.execute(46.22d);
+		rsi.execute(45.64d);
+		*/
+
+	}
+
+}
